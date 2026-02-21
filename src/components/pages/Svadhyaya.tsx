ea03b5794163bd64f0730c1e92c952f1db060ab3
Alexander Berezin <funbotan@gmail.com>
2026-02-21 18:23:39 +0100

    Returned images to the svadhyaya page

--- src/components/pages/Svadhyaya.tsx
+++ src/components/pages/Svadhyaya.tsx
@@ -1,24 +1,30 @@
-import { useTranslation } from "react-i18next";
-import { usePageNavigation } from "../Layout/Navigation";
+import {useTranslation} from "react-i18next";
+import {usePageNavigation} from "../Layout/Navigation";
 import MarkdownDisplay from "../MarkdownDisplay";
 import "./Page.css";
 
 export default function Svadhyaya() {
-  const { t } = useTranslation("pages");
+    const {t} = useTranslation("pages");
 
-  usePageNavigation({ prev: "teaching", next: "spiritual-discussion" });
+    usePageNavigation({prev: "teaching", next: "spiritual-discussion"});
 
-  return (
-    <div className="page-container">
-      <header className="page-header">
-        <h1 className="page-title font-title">{t("page09.title")}</h1>
-      </header>
+    return (
+        <div className="page-container">
+            <header className="page-header">
+                <h1 className="page-title font-title">{t("page09.title")}</h1>
+            </header>
 
-      <div className="page-main">
-        <main className="page-content svadhyaya-content">
-          <MarkdownDisplay fileName="svadhyaya" />
-        </main>
-      </div>
-    </div>
-  );
+            <div className="page-main">
+                <div className="page-side-image">
+                    <img src="/images/pratik.png" alt="Pratik"/>
+                </div>
+                <main className="page-content svadhyaya-content">
+                    <MarkdownDisplay fileName="svadhyaya"/>
+                </main>
+                <div className="page-side-image">
+                    <img src="/images/guru_white.jpeg" alt="Guru"/>
+                </div>
+            </div>
+        </div>
+    );
 }
